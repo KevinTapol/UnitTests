31eabf22f733d67864590202743de9d516e4f77b
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-03-10 20:49:06 -0500

    added my clients website for end to end test with selenium

--- sweettreatvixen.py
+++ sweettreatvixen.py
@@ -6,6 +6,8 @@
 # require
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -14,3 +16,10 @@ driver = webdriver.Chrome(service=service)
 # https://sites.google.com/chromium.org/driver/
 # https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/win64/chromedriver-win64.zip
 
+driver.get("https://sweettreatvixen.netlify.app/")
+
+input_element = driver.find_element(By.CLASS_NAME, "")
+
+time.sleep(10)
+
+driver.quit()
