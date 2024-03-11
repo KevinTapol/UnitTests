e69c143f2b74a2d38dd19fe462aed4477ce8fcdf
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-03-11 10:48:06 -0500

    XPATH, class or ID

--- sweettreatvixen.py
+++ sweettreatvixen.py
@@ -7,6 +7,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(executable_path="chromedriver.exe")
@@ -18,7 +21,15 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://sweettreatvixen.netlify.app/")
 
-input_element = driver.find_element(By.CLASS_NAME, "")
+# WebDriverWait(driver, 5).until(
+#     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Catering')]"))
+# )
+
+# not working maybe because it's a button with an href instead of an anchor tag
+# link = driver.find_element((By.XPATH, "//*[contains(text(), 'Catering')]"))
+# link.click()
+
+# input_element = driver.find_element(By.CLASS_NAME, "")
 
 time.sleep(10)
 
